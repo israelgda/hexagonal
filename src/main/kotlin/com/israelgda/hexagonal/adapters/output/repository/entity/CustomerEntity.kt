7d0703409afd6ee841a2b265f565a6c0a4532e53
Israel Gadelha <50384389+israelgda@users.noreply.github.com>
2025-01-03 00:00:15 -0300

    feat: Deletes AddressEntity and refactor usages changing to fields on CustomerEntity.

--- src/main/kotlin/com/israelgda/hexagonal/adapters/output/repository/entity/CustomerEntity.kt
+++ src/main/kotlin/com/israelgda/hexagonal/adapters/output/repository/entity/CustomerEntity.kt
@@ -1,12 +1,9 @@
 package com.israelgda.hexagonal.adapters.output.repository.entity
 
-import com.israelgda.hexagonal.application.core.domain.Address
-import com.israelgda.hexagonal.application.core.domain.Customer
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
-import jakarta.persistence.OneToOne
 import org.jetbrains.annotations.NotNull
 
 @Entity(
@@ -21,10 +18,16 @@ data class CustomerEntity(
     val name: String,
 
     @NotNull
-    @OneToOne(mappedBy = "customer")
-    var address: AddressEntity?,
+    val street: String,
+
+    @NotNull
+    val city: String,
+
+    @NotNull
+    val state: String,
 
     @NotNull
     val cpf: String,
+
     val isValidCpf: Boolean = false,
 )
--- src/main/kotlin/com/israelgda/hexagonal/application/core/domain/Customer.kt
+++ src/main/kotlin/com/israelgda/hexagonal/application/core/domain/Customer.kt
@@ -3,7 +3,9 @@ package com.israelgda.hexagonal.application.core.domain
 data class Customer(
     val id: String? = null,
     val name: String,
-    var address: Address? = null,
+    var street: String? = null,
+    var city: String? = null,
+    var state: String? = null,
     val cpf: String,
     val isValidCpf: Boolean = false,
 )
