2e364197c2f82ff1fbfc0c0fef8852d3f2c7adda
Israel Gadelha <50384389+israelgda@users.noreply.github.com>
2025-01-03 21:09:28 -0300

    FIX: Fixes mapping on AddressResponse and annotations missing on CustomerController.

--- src/main/kotlin/com/israelgda/hexagonal/adapters/input/controller/CustomerController.kt
+++ src/main/kotlin/com/israelgda/hexagonal/adapters/input/controller/CustomerController.kt
@@ -3,8 +3,10 @@ package com.israelgda.hexagonal.adapters.input.controller
 import com.israelgda.hexagonal.adapters.input.controller.request.CustomerRequest
 import com.israelgda.hexagonal.adapters.input.toCustomerInitialization
 import com.israelgda.hexagonal.application.ports.input.InsertCustomerInputPort
+import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
@@ -17,8 +19,8 @@ class CustomerController(
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    fun insert(customerRequest: CustomerRequest) {
-        with(customerRequest) {
+    fun insert(@Valid @RequestBody customerRequest: CustomerRequest) {
+        return with(customerRequest) {
             insertCustomerInputPort.insert(
                 toCustomerInitialization(),
                 zipCode
--- src/main/kotlin/com/israelgda/hexagonal/adapters/output/client/response/AddressResponse.kt
+++ src/main/kotlin/com/israelgda/hexagonal/adapters/output/client/response/AddressResponse.kt
@@ -1,8 +1,13 @@
 package com.israelgda.hexagonal.adapters.output.client.response
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class AddressResponse(
+    @JsonProperty("logradouro")
     val street: String,
+    @JsonProperty("localidade")
     val city: String,
+    @JsonProperty("estado")
     val state: String,
     val cep: String,
 )
